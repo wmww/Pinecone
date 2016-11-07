5e79cbba13ea3068cd48979ababa3b4038f2bdbe
William W Wold <wilcoe@rocketmail.com>
2016-11-07 11:05:18 -0800

    now prints lexer output

--- src/Token.cpp
+++ src/Token.cpp
@@ -1,1 +1,31 @@
+#include "../h/Token.h"
+
+string Token::typeToString(Token::Type in)
+{
+	switch (in)
+	{
+		case WHITESPACE: return "whitespace";
+		case IDENTIFIER: return "identifier";
+		case LITERAL: return "literal";
+		case OPERATOR: return "operator";
+		case COMMENT: return "comment";
+		case SCOPE: return "scope";
+		case UNKNOWN: return "UNKNOWN";
+		default: return "ERROR GETTING TOKEN TYPE";
+	}
+}
+
+string stringFromTokens(vector<Token>& tokens)
+{
+	string out;
+	
+	for (unsigned i=0; i<tokens.size(); ++i)
+	{
+		out+=tokens[i].getText() + " [" + Token::typeToString(tokens[i].getType()) + "]";
+		if (i<tokens.size()-1)
+			out+="\n";
+	}
+	
+	return out;
+}
 
