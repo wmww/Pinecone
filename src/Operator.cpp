d249e1f76dffa262cf2a32ea164a74b00dc6245a
William W Wold <wilcoe@rocketmail.com>
2016-11-09 21:50:08 -0800

    switched operator parse direction to be based on precedence parity

--- src/Operator.cpp
+++ src/Operator.cpp
@@ -4,20 +4,20 @@
 unordered_map<string, shared_ptr<OperatorData>> OperatorData::operators;
 vector<int> OperatorData::precedenceLevels;
 
-Operator opPlus=		OperatorData::create("+", 20, 20, true, true, true);
-Operator opMinus=		OperatorData::create("-", 20, 20, true, true, true);
-Operator opMultiply=	OperatorData::create("*", 30, 30, true, true, true);
-Operator opDivide=		OperatorData::create("/", 30, 30, true, true, true);
-Operator opEqual=		OperatorData::create("=", 40, 40, true, true, true);
-Operator opGreater=		OperatorData::create(">", 40, 40, true, true, true);
-Operator opLess=		OperatorData::create("<", 40, 40, true, true, true);
-Operator opColon=		OperatorData::create(":", 50, 10, false, false, false);
-Operator opComma=		OperatorData::create(",", 60, 60, true, true, false);
-Operator opDot=			OperatorData::create(".", 70, 70, true, true, false);
-Operator opIf=			OperatorData::create("?", 80, 80, true, true, false);
-Operator opLoop=		OperatorData::create("@", 90, 90, true, true, false);
-Operator opOpenPeren=	OperatorData::create("(", 100, 100, false, false, false);
-Operator opClosePeren=	OperatorData::create(")", 100, 100, false, false, false);
+const Operator opPlus=		OperatorData::create("+", 20, 20, true);
+const Operator opMinus=		OperatorData::create("-", 20, 20, true);
+const Operator opMultiply=	OperatorData::create("*", 30, 30, true);
+const Operator opDivide=	OperatorData::create("/", 30, 30, true);
+const Operator opEqual=		OperatorData::create("=", 40, 40, true);
+const Operator opGreater=	OperatorData::create(">", 40, 40, true);
+const Operator opLess=		OperatorData::create("<", 40, 40, true);
+const Operator opColon=		OperatorData::create(":", 15, 50, false);
+const Operator opComma=		OperatorData::create(",", 60, 60, false);
+const Operator opDot=		OperatorData::create(".", 70, 70, false);
+const Operator opIf=		OperatorData::create("?", 80, 80, false);
+const Operator opLoop=		OperatorData::create("@", 90, 90, false);
+const Operator opOpenPeren=	OperatorData::create("(", 100, 100, false);
+const Operator opClosePeren=OperatorData::create(")", 100, 100, false);
 
 void getOperators(string text, vector<Operator>& out)
 {
