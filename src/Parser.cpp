7a4dfd7f4b1932bcb7aeb93e4a3604a010d3bb76
William W Wold <wilcoe@rocketmail.com>
2016-11-09 21:59:57 -0800

    began implementation of recursive parser

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -5,15 +5,23 @@
 #include <vector>
 using std::vector;
 
+
+
 //	recursivly parse tokens and return action
 //		tokens: the tokens to parse
 //		left: left most token to parse (inclusinve)
 //		right: right most token to parse (inclusive)
-//		lr: if to parse left to right, right to left parsing is used if false
+//		precedence: the precedence of the operator to look for (even means left to right, odd right to left)
 //		returns: pointer to action for that section of tokens
-ActionPtr parseTokens(const vector<Token>& tokens, int left, int right, bool lr);
+ActionPtr parseTokens(const vector<Token>& tokens, int left, int right, int precedence);
 
-ActionPtr parseTokens(const vector<Token>& tokens)
+ActionPtr parseTokens(vector<Token>& tokens)
 {
-	return voidAction;
+	return parseTokens(tokens, 0, tokens.size(), OperatorData::precedenceLevels.back());
 }
+
+ActionPtr parseTokens(const vector<Token>& tokens, int left, int right, int precedence)
+{
+	
+}
+
