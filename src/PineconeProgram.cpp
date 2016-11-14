043c2c6846a01bf1b7f6d81e7bf720050341353b
Sophie Winter <git@phie.me>
2016-11-14 12:16:04 -0800

    added TypeGetAction

--- src/PineconeProgram.cpp
+++ src/PineconeProgram.cpp
@@ -40,10 +40,11 @@ void PineconeProgram::resolveProgram(bool printOutput)
 		cout << endl << "parsed abstract syntax tree:\n" << astRoot->getDescription() << endl;
 	}
 	
-	if (printOutput)
+	/*if (printOutput)
 	{
 		cout << endl << "C source code:\n" << astRoot->getCSource() << endl;
-	}
+	}*/
+	
 }
 
 void PineconeProgram::execute()
