7f9383b10a661bd7aa3d6bec9e57f43da235acd2
Sophie Winter <git@phie.me>
2016-11-15 13:11:01 -0800

    fixed double metatype problem

--- src/Actions/TypeAction.cpp
+++ src/Actions/TypeAction.cpp
@@ -8,11 +8,6 @@ public:
 	TypeGetAction(Type typeIn, string textIn):
 		Action(Type(new TypeBase(TypeBase::METATYPE, typeIn, textIn)), Void, Void, textIn)
 	{
-		if (typeIn->getType()!=TypeBase::METATYPE)
-		{
-			error.log("TypeGetAction created with type that is not a METATYPE", INTERNAL_ERROR);
-		}
-		
 		setDescription(textIn + " (type)");
 	}
 	
