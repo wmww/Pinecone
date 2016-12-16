8c66ad26e77f55e30a72adbbd7c3e14d5b4964b7
William W Wold <wilcoe@rocketmail.com>
2016-12-15 16:44:51 -0800

    started base class for AstNode and began implementing a few nodes

---
+++ src/Parser2.cpp
@@ -0,0 +1,17 @@
+
+#include "../h/Token.h"
+#include "../h/Action.h"
+#include "../h/Namespace.h"
+#include "../h/ErrorHandler.h"
+#include "../h/StackFrame.h"
+#include "../h/AllOperators.h"
+#include "../h/msclStringFuncs.h"
+
+#include <vector>
+using std::vector;
+
+using std::min;
+using std::max;
+
+extern StackFrame stdLibStackFrame;
+extern Namespace stdLibNamespace;
