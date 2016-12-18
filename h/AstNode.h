2c8a7735a0f84d1423ee53bcaebad09f9a28ba67
William W Wold <wilcoe@rocketmail.com>
2016-12-18 06:28:36 -0800

    cleaned up code a bit

--- h/AstNode.h
+++ h/AstNode.h
@@ -4,9 +4,14 @@
 #include "Action.h"
 #include "Namespace.h"
 
+#include <vector>
+using std::vector;
+
 class AstNodeBase;
 
 typedef shared_ptr<AstNodeBase> AstNode;
+
+AstNode astNodeFromTokens(const vector<Token>&, int start, int end);
 
 class AstNodeBase
 {
@@ -21,6 +26,9 @@ public:
 class AstVoid: public AstNodeBase
 {
 public:
+	
+	shared_ptr<AstVoid> make() {return shared_ptr<AstVoid>(new AstVoid);}
+	
 	string getString() {return "void";}
 	Type getReturnType(Namespace ns) {return Void;}
 	Action getAction(Namespace ns) {return voidAction;}
@@ -32,6 +40,10 @@ class AstList: public AstNodeBase
 {
 public:
 	
+	//	make a new instance of this type of node
+	shared_ptr<AstList> make() {return shared_ptr<AstList>(new AstList);}
+	
+	//	add a sub node to the end of the list
 	void addNode(AstNode in) {nodes.push_back(in);}
 	
 	string getString();
@@ -40,6 +52,8 @@ public:
 	Action getAction(Namespace ns);
 	
 private:
+	
+	//	the list of sub nodes
 	vector<AstNode> nodes;
 };
 
@@ -47,6 +61,8 @@ class AstExpression: public AstNodeBase
 {
 public:
 	
+	//	make a new instance of this type of node
+	/*
 	AstNode make(AstNode leftInIn, Token tokenIn, AstNode rightInIn)
 	{
 		AstExpression* node=new AstExpression;
@@ -57,18 +73,23 @@ public:
 		
 		return AstNode(node);
 	}
-	
-	/*
-	AstExpression(AstNode leftInIn, Token tokenIn, AstNode rightInIn)
-	{
-		leftIn=leftInIn;
-		token=tokenIn;
-		rightIn=rightInIn;
-	}
 	*/
 	
-	void setLeftIn(AstNode in) {leftIn=in;}
-	void setRightIn(AstNode in) {rightIn=in;}
+	shared_ptr<AstExpression> make(AstNode leftInIn, Token tokenIn, AstNode rightInIn)
+	{
+		shared_ptr<AstExpression> node(new AstExpression);
+		
+		node->leftIn=leftInIn;
+		node->token=tokenIn;
+		node->rightIn=rightInIn;
+		
+		return node;
+	}
+	
+	
+	//	set the inputs of this node
+	//void setLeftIn(AstNode in) {leftIn=in; action=nullptr;}
+	//void setRightIn(AstNode in) {rightIn=in;}
 	//void setToken(Token in) {token=in;}
 	
 	string getString();
@@ -82,6 +103,7 @@ private:
 	
 	AstExpression() {}
 	
+	//	the action this node maps to, starts as nullptr and is resolved 
 	Action action=nullptr;
 	Token token=nullptr;
 	AstNode leftIn=nullptr, rightIn=nullptr;
